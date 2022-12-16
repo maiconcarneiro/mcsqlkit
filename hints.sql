8e0341230eae327afe3cab70d1e6af0057b56708
Maicon Carneiro <maicon116@gmail.com>
2022-12-16 10:24:35 -0300

    adapted for non-SYS users

--- hints.sql
+++ hints.sql
@@ -1,14 +1,13 @@
-set lines 155
-col hint for a150
+set lines 400
+col hint for a200
 select hint from (
 select p.name, p.signature, p.category,
-       row_number()
-         over (partition by sd.signature, sd.category order by sd.signature) row_num,
+       row_number() over (partition by sd.signature, sd.category order by sd.signature) row_num,
        extractValue(value(t), '/hint') hint
-from sqlobj$data sd, dba_sql_profiles p,
+from sys.sqlobj$data sd, sys.dba_sql_profiles p,
      table(xmlsequence(extract(xmltype(sd.comp_data),
                                '/outline_data/hint'))) t
-where sd.	 = 1
+where sd.obj_type = 1
 and p.signature = sd.signature
 and p.name like nvl('&1',name)
 )
