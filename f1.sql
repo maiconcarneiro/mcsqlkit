00745d1f13f8d712669993ca36022d9e3162a866
maiconcarneiro <maicon116@gmail.com>
2025-06-03 19:32:37 -0300

    login fixed

--- f1.sql
+++ f1.sql
@@ -1,8 +1,10 @@
-set verify off
-set termout off
-column NODE new_value vNODE 
-column CNAME new_value vCNAME 
-SELECT instance_name AS NODE, sys_context('USERENV','DB_NAME') as CNAME  FROM V$INSTANCE;
-SET termout ON
+set feedback off;
+set termout off;
+COLUMN FORMAT_LINE NEW_VALUE _FORMAT_LINE
+SELECT CASE WHEN  sys_context('USERENV','MODULE') = 'SQLcl'
+            THEN '@|blue _USER|@@@|&_CON_NAME_COLOR &vCNAME|@@|white > |@'
+            ELSE q'[_USER'@'&vCNAME> ]'
+        END FORMAT_LINE
+FROM DUAL;
 
-set sqlprompt '@|blue _USER|@@@|&_CON_NAME_COLOR &vCNAME|@@|white > |@';
+set sqlprompt "&&_FORMAT_LINE";
