026c06d267f50f31430820e548011b6167a4e4f4
maiconcarneiro <maicon116@gmail.com>
2025-06-05 10:46:04 -0300

    f1 fixed

--- f1.sql
+++ f1.sql
@@ -3,7 +3,7 @@ set termout off;
 COLUMN FORMAT_LINE NEW_VALUE _FORMAT_LINE
 SELECT CASE WHEN  sys_context('USERENV','MODULE') = 'SQLcl'
             THEN '@|blue _USER|@@@|&_CON_NAME_COLOR &vCNAME|@@|white > |@'
-            ELSE q'[_USER'@'&vCNAME> ]'
+            ELSE q'[_USER'@'_CONNECT_IDENTIFIER> ]'
         END FORMAT_LINE
 FROM DUAL;
 
